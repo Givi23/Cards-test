b808c351662033fe37fa98ee1d61120b1943a092
Givi23 <givi023@mail.ru>
2023-05-30 21:10:25 +0300

    add const

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+document.querySelectorAll(".slide");
+console.log(document.querySelectorAll(".slide"))
+let fruits = ["apple", "orange","lemon", "grape"];
+
+const slides = document.querySelectorAll(".slide");
+console.log(slides);
